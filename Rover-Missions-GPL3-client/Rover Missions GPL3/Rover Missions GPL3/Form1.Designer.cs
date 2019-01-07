11bcd2097214246987b0391ce3272be452cf91b2
we6jbo <we6jbo@gmail.com>
2019-01-06 23:54:47 -0800

    This is the client side interface for the game.

---
+++ Rover-Missions-GPL3-client/Rover Missions GPL3/Rover Missions GPL3/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Rover_Missions_GPL3
+{
+    partial class RoverMissionsGPL3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RoverMissionsGPL3_web = new System.Windows.Forms.WebBrowser();
+            this.openLicense = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RoverMissionsGPL3_web
+            // 
+            this.RoverMissionsGPL3_web.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RoverMissionsGPL3_web.Location = new System.Drawing.Point(0, 0);
+            this.RoverMissionsGPL3_web.MinimumSize = new System.Drawing.Size(20, 20);
+            this.RoverMissionsGPL3_web.Name = "RoverMissionsGPL3_web";
+            this.RoverMissionsGPL3_web.Size = new System.Drawing.Size(1322, 847);
+            this.RoverMissionsGPL3_web.TabIndex = 0;
+            this.RoverMissionsGPL3_web.Url = new System.Uri("http://jeremiahonealts.com/RoverMissionsGPL3-web.php", System.UriKind.Absolute);
+            // 
+            // openLicense
+            // 
+            this.openLicense.Location = new System.Drawing.Point(1221, 812);
+            this.openLicense.Name = "openLicense";
+            this.openLicense.Size = new System.Drawing.Size(75, 23);
+            this.openLicense.TabIndex = 1;
+            this.openLicense.Text = "License";
+            this.openLicense.UseVisualStyleBackColor = true;
+            this.openLicense.Click += new System.EventHandler(this.openLicense_Click);
+            // 
+            // RoverMissionsGPL3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1322, 847);
+            this.Controls.Add(this.openLicense);
+            this.Controls.Add(this.RoverMissionsGPL3_web);
+            this.Name = "RoverMissionsGPL3";
+            this.Text = "Rover Missions GPL3 - General Public License V3 ( https://www.gnu.org/licenses/gp" +
+    "l-3.0.en.html)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser RoverMissionsGPL3_web;
+        private System.Windows.Forms.Button openLicense;
+    }
+}
+
---
+++ Rover-Missions-GPL3-client/Rover Missions GPL3/Rover Missions GPL3/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+/*
+ Source code: https://github.com/we6jbo/Rover-Missions-GPL3
+Rover Missions GPL3 - This is the client side of the Rover Missions interface. The interface that allows the user to move the rover around and to perform missions.
+    Copyright (C) 2019  Jeremiah O'Neal and Natalie O'Neal
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rover_Missions_GPL3
+{
+    public partial class RoverMissionsGPL3 : Form
+    {
+        public RoverMissionsGPL3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openLicense_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Source code: https://github.com/we6jbo/Rover-Missions-GPL3\nRover Missions GPL3 - This is the client side of the Rover Missions interface. The interface that allows the user to move the rover around and to perform missions.\nCopyright(C) 2019  Jeremiah O'Neal and Natalie O'Neal\n\nThis program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program.If not, see<https://www.gnu.org/licenses/>.");
+        }
+    }
+}
